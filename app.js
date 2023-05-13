78a4ebe0922cc24a906c4aa55fef53cf44d7748e
JuanFontalvo <fontalvojuan098@gmail.com>
2023-05-12 21:33:00 -0500

    Añado Html con estructura basica, Css con diseño bas y js sin contenido

---
+++ app.js
@@ -0,0 +1,5 @@
+document.querySelector('img').onClick = function() {
+    var image = document.getElementsByClassName('imagePerfil');
+    image.style.width = '500px';
+    image.style.height = 'auto';
+}
