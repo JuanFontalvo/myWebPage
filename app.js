fa8323aa38e1b46b435ce19b8a8a990bc8290e42
JuanFontalvo <fontalvojuan098@gmail.com>
2023-05-12 22:07:38 -0500

    Correccion Css

--- app.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('img').onClick = function() {
-    var image = document.getElementsByClassName('imagePerfil');
-    image.style.width = '500px';
-    image.style.height = 'auto';
-}
